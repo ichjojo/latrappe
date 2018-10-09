265c1728e33f098967be87b240923bed5585f547
ubuntu_vivado2017_4 <ichjojo@163.com>
2018-10-09 15:05:13 +0800

    modify build.sh again+2

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,13 @@
 
 cd /home/flu/peta_proj/github/latrappe
 
-sudo su flu
+#sudo su flu
 #sudo su flu <<!
 #year2000
 #!
 #su - flu -c petalinux-build
+
+sudo su flu
 petalinux-build
 if [ $? -ne 0 ]; then
         echo "Building failed!!"
