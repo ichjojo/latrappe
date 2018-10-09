a379a48ac0839f083851b67557b63bbbbe4d2aae
ubuntu_vivado2017_4 <ichjojo@163.com>
2018-10-09 14:58:26 +0800

    modify build.sh again+1

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,9 +2,10 @@
 
 cd /home/flu/peta_proj/github/latrappe
 
-sudo su flu <<!
-year2000
-!
+sudo su flu
+#sudo su flu <<!
+#year2000
+#!
 #su - flu -c petalinux-build
 petalinux-build
 if [ $? -ne 0 ]; then
