c48b9f02631806f6278e2523bf0761ea75e4c27b
ubuntu_vivado2017_4 <ichjojo@163.com>
2018-10-09 15:40:19 +0800

    again+7

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,9 +9,28 @@
 #!
 #su - flu -c petalinux-build
 
-sudo su
+echo "the current working directory is:"
+pwd
+echo "the current working groups are:"
+groups
+echo "the current user is:"
+whoami
+
+echo ""
+echo "start to switch user to flu......"
+echo ""
 sudo su flu
 cd /home/flu/peta_proj/github/latrappe
+
+sleep 5s
+
+echo "the current working directory is:"
+pwd
+echo "the current working groups are:"
+groups
+echo "the current user is:"
+whoami
+
 #petalinux-build
 if [ $? -ne 0 ]; then
         echo "Building failed!!"
