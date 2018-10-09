afbe58a15f0d442414e97fef82519237f272c4f2
ubuntu_vivado2017_4 <ichjojo@163.com>
2018-10-09 15:22:04 +0800

    again+5

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 
 #sudo su - flu
-cd /home/flu/peta_proj/github/latrappe
+#cd /home/flu/peta_proj/github/latrappe
 
 #sudo su flu
 #sudo su flu <<!
 #year2000
 #!
-su - flu -c petalinux-build
+#su - flu -c petalinux-build
 
-#sudo su flu
-#petalinux-build
+sudo su
+sudo su flu
+cd /home/flu/peta_proj/github/latrappe
+petalinux-build
 if [ $? -ne 0 ]; then
         echo "Building failed!!"
         exit 1
